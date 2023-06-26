1eb94e72684680841d0e9a5994ed30a7022fcb75
BarbaraGB1 <bargarci@c1r6s1.42madrid.com>
2023-06-26 10:52:16 +0200

    push

--- push.c
+++ push.c
@@ -1,9 +1,10 @@
 #include "libft/libft.h"
 #include "push_swap.h"
 
-void push(t_list **lst, t_list **second)
+void	push(t_list **lst, t_list **second)
 {
-	t_list *tmp;
+	t_list	*tmp;
+
 	if (!lst || !second)
 		return ;
 	tmp = *lst;
